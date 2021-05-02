c1aecebab80ea9a2816c9b45d03b03147cb28c21
heriprastiio <heri.prastio56@gmail.com>
2021-05-02 22:17:57 +0700

    Add host

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("Tes Data");
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server(
+        {
+            port: 5000,
+            host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.47.247',
+            routes:{
+                cors:{
+                    origin: ['*'],
+                },
+            },
+        },
+    );
+}
